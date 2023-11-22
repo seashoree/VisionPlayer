63c1a7c6547eb9fab833dca904ee7401b7d66ccd
seashoree <51018064+seashoree@users.noreply.github.com>
2023-11-23 00:48:38 +0100

    Add files via upload

---
+++ pic2eyes.py
@@ -0,0 +1,10 @@
+import gaze from "gaze-detection"
+#https://github.com/charliegerard/gaze-detection: 直接进行眼睛探测，快速完成开发
+
+from gaze_tracking import GazeTracking
+#https://github.com/antoinelame/GazeTracking ： 直接进行眼睛关注点探测，完成开发
+
+
+#https://github.com/pathak-ashutosh/Eye-blink-detection
+
+#https://github.com/Chris10M/open-eye-detector
